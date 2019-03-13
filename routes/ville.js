2b4f1e79af827ae4f6879bdd14ea50e0f047f221
thibault <nthibault@myges.fr>
2019-03-13 17:19:28 +0100

    Question 1 Finie

--- routes/ville.js
+++ routes/ville.js
@@ -1,9 +1,20 @@
 var express = require('express');
+var axios = require('axios');
 var router = express.Router();
 
 /* GET Ville page */
 router.post('/', function(req, res, next) {
-  res.render('ville', { ville:req.body.nom_ville});
+
+  //Ici on veut lancer une autre requête vers la map. Le but est de récupérer les informations
+  axios.get('https://geocode.xyz/'+req.body.nom_ville+'?json=1&auth=889202541745294745990x1974').then(function (response) {
+
+    console.log(req.body.nom_ville)
+    res.render('ville',{ville:'ville : '+response.data.standard.city+' latitude : '+response.data.latt + 'longitude'+response.data.longt});
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
 
 });
 
