95b6c48e27b66ed7be33c2a3fbac5da04dbbcdb4
thibault <nthibault@myges.fr>
2019-03-13 14:29:07 +0100

    Get Ville

--- routes/ville.js
+++ routes/ville.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET Ville page */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Node App' });
+  res.render('ville', { ville:req.query.nom_ville});
+  
 });
 
 
