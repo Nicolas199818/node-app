a9d452b0d6211e6be345c5c77d07fe0c96a99c3a
thibault <nthibault@myges.fr>
2019-03-13 14:46:03 +0100

    Post Ville

--- routes/ville.js
+++ routes/ville.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET Ville page */
-router.get('/', function(req, res, next) {
-  res.render('ville', { ville:req.query.nom_ville});
-  
+router.post('/', function(req, res, next) {
+  res.render('ville', { ville:req.body.nom_ville});
+
 });
 
 
